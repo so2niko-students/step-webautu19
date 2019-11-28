1f0117e24cc6333ffbf86295bc91fafb55b437ff
Николай Сотула <sotula@itstep1.dp.ua>
2019-11-28 21:15:58 +0200

    js intro lection

---
+++ 20191128_js_intro/app/main.js
@@ -0,0 +1,28 @@
+// console.log('Hell  - o!!!');
+// console.log(22);
+
+// console.log(inn);
+// console.log(22.33);
+
+let inn = 28374666433; // число - Number
+let name = 'Николай'; //String - строка
+let isGoodDay = true; //Boolean - логический
+let blackFriday = null;//null - Тип
+let neyavno;
+let ooo = {
+    aaa : 'Оооооо'
+}; // Object
+
+console.log(name);
+console.log(typeof name);
+console.log(inn);
+console.log(typeof inn);
+console.log(isGoodDay);
+console.log(typeof isGoodDay);
+console.log(neyavno);
+console.log(typeof neyavno);
+console.log(blackFriday);
+console.log(typeof blackFriday);
+console.log(ooo);
+console.log(typeof ooo);
+
