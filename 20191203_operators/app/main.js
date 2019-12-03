c821c01b7e6c70212f1c8f85e992b9557c4acccd
Николай Сотула <sotula@itstep1.dp.ua>
2019-12-03 21:01:10 +0200

    js operators

---
+++ 20191203_operators/app/main.js
@@ -0,0 +1,57 @@
+var id = 11;
+
+let name;
+let surname = 'Pupkin';
+
+const age = 112;
+/*
+console.log(name, typeof name);
+console.log(surname, typeof surname);
+console.log(age);
+*/
+name = 'Vasya';
+console.log(name);
+// age = 3;
+
+/*
+В имени переменной может быть:
+a-z
+A-Z
+0-9
+_
+$ - не рекомендую(@Николай)
+# - не рекомендую(@Николай)
+
+Переменная должна начинаться с буквы или _ или $
+(Нельзя начинать с цифры!!!)
+
+krasavchik - bad practice!
+a
+b
+c
+d
+e
+f
+h
+g
+
+*/
+
+//ОПЕРАТОРЫ
+
+//Присвоения  =
+// переменная = значение;
+
+/*Математические операторы
++
+-
+*
+/
+
+*/
+
+let animalAge = 11;
+
+let dogAge = animalAge++;
+console.log('dogAge', dogAge);//12
+console.log('animalAge', animalAge);//12
