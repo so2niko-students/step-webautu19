23c6e25f1fa7f8868c1798804fa4f2fb09e11048
Nick Sotula <so2niko@gmail.com>
2020-05-19 19:59:15 +0300

    feat: add ctx into innerHTML

--- 20200519-repeat_themes/app/main.js
+++ 20200519-repeat_themes/app/main.js
@@ -1,28 +1,41 @@
 const ctxMenu = {
-    link : document.querySelector('.ctx-menu'),
+    container : document.querySelector('.ctx-menu-container'),
     isShow : false
 };
 
 function onBodyClickCtxMenu(ev){
     ev.preventDefault();
-    const hide = 'd-none';
-    const PX = 'PX';
 
-    ctxMenu.link.style.left = ev.clientX + PX;
-    ctxMenu.link.style.top = ev.clientY + PX;
+    //? Вариант с удалением с body
+    // if(ctxMenu.isShow){
+    //     const prevCtx = document.querySelector('.ctx-menu');
+    //     document.body.removeChild(prevCtx);
+    // }   
+
+    ctxMenu.container.innerHTML = `
+    <div class="w-200 pos-fix ctx-menu" 
+        style="top: ${ev.clientY}px; left: ${ev.clientX}px">
+        <ul class="list-group user-select-none">
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-folder-plus"></i> Создать папку</li>
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-file-upload"></i> Загрузка файлов</li>
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-folder-open"></i> Загрузить папку</li>
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-file-word"></i> Google Документы</li>
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-file-excel"></i> Google Таблицы</li>
+            <li class="list-group-item p-1 pl-2"><i class="fas fa-file-powerpoint"></i> Google Презентации</li>
+            <li class="list-group-item p-1 pl-2">Ещё</li>
+        </ul>
+    </div>`;
 
     ctxMenu.isShow = true;
-    ctxMenu.link.classList.remove(hide);
 }
 
 function onBodyClick(){
-    const hide = 'd-none';
-
     if(ctxMenu.isShow){
-        ctxMenu.link.classList.add(hide);
+        ctxMenu.container.innerHTML = '';
         ctxMenu.isShow = false;
     }    
 }
-
-document.body.addEventListener('contextmenu', onBodyClickCtxMenu);
-document.body.addEventListener('click', onBodyClick);
+(_ => {
+    document.body.addEventListener('contextmenu', onBodyClickCtxMenu);
+    document.body.addEventListener('click', onBodyClick);
+})();
