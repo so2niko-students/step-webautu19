323e29115c813f33fbaf19c8594ecfdaf2101471
Nick Sotula <so2niko@gmail.com>
2020-08-04 19:45:48 +0300

    feat: add products to sending tg message

--- 20200723_shop_cart/app/components/form/model_form.js
+++ 20200723_shop_cart/app/components/form/model_form.js
@@ -1,6 +1,6 @@
 export default class ModelForm{
     products = [];
-    botUrl = 'https://api.telegram.org/bot1362251820:AAHlMA0ngWHlz123TRJlTfmkBiZVVetf-h0/sendMessage?chat_id='
+    botUrl = 'https://api.telegram.org/bot1362251820:AAHlMA0ngWHlz123TRJlTfmkBiZVVetf-h0/sendMessage?parse_mode=MarkdownV2&chat_id='
     chatId = '-353593305';
 
     constructor(){
@@ -9,11 +9,20 @@ export default class ModelForm{
 
     setProductCart(products){
         this.products = products;
+        console.log(products);
     }
 
     sendOrder(userData){
         const { email, name, tel } = userData;
-        const url = `${ this.botUrl }${ this.chatId }&text=email:${ email };name:${ name };tel:${ tel }`;
+        const products = this.products.map(({ title, price, count, id }) => {
+            return `\*id\*: ${ id }; \*title\*: ${ title }; \*price\*: ${ price }; \*count\*: ${ count }`;
+        }).join(`\n`);
+        console.log(products);
+
+        const email2 = email.replace(/\./g, '\\.');
+        console.log(email2);
+        
+        const url = `${ this.botUrl }${ this.chatId }&text=\*email\*: ${ email2 }; \*name\*: ${ name }; \*tel\*: ${ tel }; \*products\*: ${ products }`;
         fetch(url);
     }
 }
