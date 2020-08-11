3cd72b647fa74e91ec9877e08fe0aa36781d263e
Nick Sotula <so2niko@gmail.com>
2020-08-11 18:48:21 +0300

    feat: animated notidication

--- 20200723_shop_cart/app/components/notification/view_notification.js
+++ 20200723_shop_cart/app/components/notification/view_notification.js
@@ -1,8 +1,16 @@
 export default class ViewNotification{
     notificationDOM = document.querySelector('.notification');
+    showClass = 'show_notification';
 
     renderAddToCart(){
         this.notificationDOM.innerHTML = '<strong>ДОБАВЛЕН В КОРЗИНУ</strong>';
-        setTimeout(() => this.notificationDOM.innerHTML = '', 3000);
+        this.notificationDOM.classList.add(this.showClass);
+
+        setTimeout(() => this.notificationDOM.classList.remove(this.showClass) = '', 4000);
     }
+    /**
+     * 2sec - up
+     * 2sec - wait
+     * 2sec - down
+     */
 }
